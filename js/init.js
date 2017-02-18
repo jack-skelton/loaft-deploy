947b1d18d49eb6479e0cb14a19f09beaaa863d32
jack-skelton <jack.s1995@live.com.au>
2017-02-19 01:11:54 +1100

    update

--- js/init.js
+++ js/init.js
@@ -4,15 +4,15 @@ $(function(){
         $(".element").typed({
         //strings: ["web developers.", "graphic designers.","solution architects."],
         //"modern <strong>web</strong> and <strong>fraphic</strong> design. "<strong>digital</strong> specialists and <strong>growth</strong> hackers"
-        strings: ["modern <strong>web</strong> and <strong>graphic</strong> design.", "<strong>digital</strong> specialists and <strong>growth</strong> hackers"],
+        strings: ["modern <strong>web</strong> and <strong>graphic</strong> design", "<strong>digital</strong> specialists and <strong>growth</strong> hackers", "a design studio called <strong>Loaft.</strong>"],
         typeSpeed: 30,
-        loop: true,
+        loop: false,
         showCursor: true,
-        backDelay: 3500,
-        startDelay: 0,
-        preStringTyped: function() {
+        backDelay: 2000,
+        startDelay: 0
+        /*preStringTyped: function() {
             $("#typer").toggleClass("typer-change")
-        }
+        }*/
     })
 });
 
@@ -27,38 +27,23 @@ $(function(){
 	new ScrollMagic.Scene({triggerElement: "#typer", triggerHook: 0, offset: 100})
         .setClassToggle("nav", "scrolled-landing") // add class toggle
         .addTo(controller);
+    new ScrollMagic.Scene({triggerElement: "#about", triggerHook: 0.9})
+        .setClassToggle("#about", "show") // add class toggle
+        .addTo(controller);
+    new ScrollMagic.Scene({triggerElement: "#services", triggerHook: 0.9})
+        .setClassToggle("#services", "show") // add class toggle
+        .addTo(controller);
+    new ScrollMagic.Scene({triggerElement: "#work", triggerHook: 0.60})
+        .setClassToggle("#work", "show") // add class toggle
+        .addTo(controller)
+
+    
 });
 
 $(document).ready(function(){
     $('.scrollspy').scrollSpy({scrollOffset: 0});
 });
 
-
-/**
- * $(function(){
-      $(".dingo").typed({
-        //strings: ["web developers.", "graphic designers.","solution architects."],
-        strings: ["loaft"],
-        typeSpeed: 30,
-        loop: false,
-        showCursor: true,
-        backDelay: 3000,
-        startDelay: 500,
-        callback: function(){
-          this.showCursor = false;
-          $( ".typed-cursor" ).remove();
-          $(".element").typed({
-            //strings: ["web developers.", "graphic designers.","solution architects."],
-            strings: ["modern <strong>Web</strong> and <strong>Graphic</strong> design.", "<strong>digital</strong> specialists and <strong>growth</strong> hackers", ],
-            typeSpeed: 30,
-            loop: true,
-            showCursor: true,
-            backDelay: 3500,
-            startDelay: 1000
-          })}
-      });
-    });
- */
 
 
 $(document).ready(function() {     
